b682fa1d2429dd3b4c0efbdc6be20f71bf19bc03
Wali-Dexter <luckywali0986@gmail.com>
2022-11-15 22:14:18 +0500

    add js file and also add style and html as per requirement

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+import Dots from './images/dots.png';
+import Enter from './images/enter.png';
+import Refresh from './images/refresh.png';
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
-}
+const refreshContainer = document.querySelector('.refresh-list');
+const refresh = new Image();
+refresh.src = Refresh;
+refreshContainer.innerHTML = `<img src=${Refresh} class="refresh-icon" />`;
 
-document.body.appendChild(component());
+const enterContainer = document.querySelector('.enter-list');
+const enter = new Image();
+enter.src = Enter;
+enterContainer.innerHTML = `<img src=${Enter} class="enter-icon" />`;
+
+const listContainer = document.querySelector('.list-list');
+const dots = new Image();
+dots.src = Dots;
+
+const tasks = [
+  { index: 0, description: 'Read for 30 Minutes', completed: false },
+  { index: 1, description: 'Exersisce for 30 Minutes', completed: false },
+  { index: 2, description: 'Complete Today Projects', completed: false },
+];
+
+const createTask = (tasks) => {
+  const taskContent = `
+  <li class="element">
+      <input type="checkbox" id="checkbox" class="checkbox" />
+      <div>${tasks.description}</div>
+      <div class="dots-container"><img class="dots"src=${Dots} /></div>
+  </li>`;
+
+  listContainer.innerHTML += taskContent;
+};
+
+const createList = (arr) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    createTask(arr[i]);
+  }
+};
+
+createList(tasks);
