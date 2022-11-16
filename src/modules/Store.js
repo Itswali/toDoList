502f4c8202267bcdb527d5194a006413ae8c0fcc
Wali-Dexter <luckywali0986@gmail.com>
2022-11-16 15:29:17 +0500

    set local storage to add and store lists

--- src/modules/Store.js
+++ src/modules/Store.js
@@ -1,3 +1,27 @@
 export default class Store {
+    constructor() {
+        this.count = this.getlist().length;
+    }
+    getlist() {
+        if(localStorage.getItem('list') === null) {
+            this.list = [];
+        } else {
+            this.list = JSON.parse(localStorage.getItem('list'));
+        }
+        return this.list;
+    }
+
+    addTask(task) {
+        const newTask = {
+            id:this.count,
+            description: task.description,
+            completed: false,
+        };
+
+        const list = this.getlist();
+        list.push(newTask);
+        localStorage.setItem('list' , JSON.stringify(list));
+        this.count+=1;
+    }
 
 }
