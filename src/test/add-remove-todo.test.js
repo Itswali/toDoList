2dbd0ac762c13db4a68f33becbbdc6a60ee4fd22
Wali-Muhammad <luckywali0986@gmail.com>
2022-12-14 19:25:36 +0500

    Update add-remove-todo.test.js

--- src/test/add-remove-todo.test.js
+++ src/test/add-remove-todo.test.js
@@ -1,13 +1,11 @@
 import Todo from '../TodoClass.js';
-
 describe('Test add functionality', () => {
   test('Add 1 todo Item', () => {
     const todo = new Todo('Make tests for this!!');
     expect(Todo.getAllTodos().length).toBe(1);
     todo.deleteTodo();
   });
-  });
-
+});
 describe('Test remove functionality', () => {
   test('Remove todo task selected by the user', () => {
     const removeTask = new Todo('ensure project test work without errors!');
